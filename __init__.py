5d5af3f08a6d710d773a740729b4e8e07789e73a
florianhepp <florian.hepp1@gmail.com>
2018-01-26 17:02:56 +0100

    refactor imports

--- __init__.py
+++ __init__.py
@@ -20,7 +20,9 @@ import numpy as np
 import hashlib
 
 from datetime import datetime, timedelta
-from apriori import runApriori, dataFromFile
+
+from itertools import chain, combinations
+from collections import defaultdict
 
 from sklearn.cluster import DBSCAN, AffinityPropagation
 from sklearn.preprocessing import StandardScaler
@@ -399,9 +401,125 @@ def compute_DBSCAN(features):
     labels = db.labels_
     return core_samples_mask, labels
 
+def subsets(arr):
+    """ Returns non empty subsets of arr"""
+    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
 
-def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
-    my_habit_manager = HabitsManager()
+
+def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
+    """calculates the support for items in the itemSet and returns a subset
+   of the itemSet each of whose elements satisfies the minimum support"""
+    _itemSet = set()
+    localSet = defaultdict(int)
+
+    for item in itemSet:
+        for transaction in transactionList:
+            if item.issubset(transaction):
+                freqSet[item] += 1
+                localSet[item] += 1
+
+    for item, count in localSet.items():
+        support = float(count) / len(transactionList)
+
+        if support >= minSupport:
+            _itemSet.add(item)
+
+    return _itemSet
+
+
+def joinSet(itemSet, length):
+    """Join a set with itself and returns the n-element itemsets"""
+    return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
+
+def getItemSetTransactionList(data_iterator):
+    transactionList = list()
+    itemSet = set()
+    for record in data_iterator:
+        transaction = frozenset(record)
+        transactionList.append(transaction)
+        for item in transaction:
+            itemSet.add(frozenset([item]))  # Generate 1-itemSets
+    return itemSet, transactionList
+
+
+def runApriori(data_iter, minSupport, minConfidence):
+    """
+    run the apriori algorithm. data_iter is a record iterator
+    Return both:
+     - items (tuple, support)
+     - rules ((pretuple, posttuple), confidence)
+    """
+    itemSet, transactionList = getItemSetTransactionList(data_iter)
+
+    freqSet = defaultdict(int)
+    largeSet = dict()
+    # Global dictionary which stores (key=n-itemSets,value=support)
+    # which satisfy minSupport
+
+    assocRules = dict()
+    # Dictionary which stores Association Rules
+
+    oneCSet = returnItemsWithMinSupport(itemSet,
+                                        transactionList,
+                                        minSupport,
+                                        freqSet)
+
+    currentLSet = oneCSet
+    k = 2
+    while (currentLSet != set([])):
+        largeSet[k - 1] = currentLSet
+        currentLSet = joinSet(currentLSet, k)
+        currentCSet = returnItemsWithMinSupport(currentLSet,
+                                                transactionList,
+                                                minSupport,
+                                                freqSet)
+        currentLSet = currentCSet
+        k = k + 1
+
+    def getSupport(item):
+        """local function which Returns the support of an item"""
+        return float(freqSet[item]) / len(transactionList)
+
+    toRetItems = []
+    for key, value in largeSet.items():
+        toRetItems.extend([(tuple(item), getSupport(item))
+                           for item in value])
+
+    toRetRules = []
+    for key, value in largeSet.items()[1:]:
+        for item in value:
+            _subsets = map(frozenset, [x for x in subsets(item)])
+            for element in _subsets:
+                remain = item.difference(element)
+                if len(remain) > 0:
+                    confidence = getSupport(item) / getSupport(element)
+                    if confidence >= minConfidence:
+                        toRetRules.append(((tuple(element), tuple(remain)),
+                                           confidence))
+    return toRetItems, toRetRules
+
+
+def printResults(items, rules):
+    """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
+    for item, support in sorted(items, key=lambda (item, support): support):
+        print "item: %s , %.3f" % (str(item), support)
+    print "\n------------------------ RULES:"
+    for rule, confidence in sorted(rules, key=lambda (rule, confidence): confidence):
+        pre, post = rule
+        print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
+
+
+def dataFromFile(fname):
+    """Function which reads from the file and yields a generator"""
+    file_iter = open(fname, 'rU')
+    for line in file_iter:
+        line = line.strip().rstrip(',')  # Remove trailing comma
+        record = frozenset(line.split(','))
+        yield record
+
+
+def get_habits(logs_file_path='logs.json', min_supp=0.05, min_confidence=0.8):
     hashes_temp = []
     table_csv = []
     date_time_obj0 = datetime.strptime('2018-01-01 00:00:00.0', '%Y-%m-%d %H:%M:%S.%f')
@@ -420,7 +538,7 @@ def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
                 hashes_temp = []
                 hashes_temp.append(hash)
                 date_time_obj0 = date_time_obj1
-            else :
+            else:
                 hashes_temp.append(hash)
 
     del table_csv[0]
@@ -468,7 +586,7 @@ def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
 
     # format habits as expected and register them
     intents = []
-    
+
     for habit in habits:
         for intent in habit:
             intent = {
@@ -477,6 +595,6 @@ def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
                 'parameters': json.loads(intent)['parameters']
             }
             intents.append(intent)
-        my_habit_manager.register_habit("skill", intents)
+        # register_habit("skill", intents)
         print intents
         intents = []
