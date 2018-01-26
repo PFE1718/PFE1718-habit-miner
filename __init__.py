7452ab8de351446c2e43e1f95b8b4dd48414d405
florianhepp <florian.hepp1@gmail.com>
2018-01-26 14:33:04 +0100

    Refactor apriori method

--- __init__.py
+++ __init__.py
@@ -401,6 +401,7 @@ def compute_DBSCAN(features):
 
 
 def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
+    my_habit_manager = HabitsManager()
     hashes_temp = []
     table_csv = []
     date_time_obj0 = datetime.strptime('2018-01-01 00:00:00.0', '%Y-%m-%d %H:%M:%S.%f')
@@ -476,6 +477,6 @@ def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
                 'parameters': json.loads(intent)['parameters']
             }
             intents.append(intent)
-        register_habit("skill", intents)
+        my_habit_manager.register_habit("skill", intents)
         print intents
         intents = []
