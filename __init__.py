822af5ea9557df84897ff2342c644d1788fe156f
florianhepp <florian.hepp1@gmail.com>
2018-01-29 16:08:19 +0100

    Change csv input location and refactor apriori code for PEP8

--- __init__.py
+++ __init__.py
@@ -17,6 +17,7 @@ import json
 import random
 import csv
 import numpy as np
+import os
 import hashlib
 
 from datetime import datetime, timedelta
@@ -42,7 +43,6 @@ __author__ = 'Nuttymoon'
 # Logger: used for debug lines, like "LOGGER.debug(xyz)". These
 # statements will show up in the command line when running Mycroft.
 LOGGER = getLogger(__name__)
-
 
 class HabitsManager(object):
     """
@@ -333,8 +333,6 @@ def write_habit(X, labels,core_samples_mask_dbscan):
 
 
 
-
-
 # MAIN STEPS
 # Learning steps
 
@@ -401,106 +399,106 @@ def compute_DBSCAN(features):
     labels = db.labels_
     return core_samples_mask, labels
 
+
 def subsets(arr):
     """ Returns non empty subsets of arr"""
     return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
 
 
-def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
+def return_item_with_min_support(item_set, transaction_list, min_support, freq_set):
     """calculates the support for items in the itemSet and returns a subset
    of the itemSet each of whose elements satisfies the minimum support"""
     _itemSet = set()
-    localSet = defaultdict(int)
+    local_set = defaultdict(int)
 
-    for item in itemSet:
-        for transaction in transactionList:
+    for item in item_set:
+        for transaction in transaction_list:
             if item.issubset(transaction):
-                freqSet[item] += 1
-                localSet[item] += 1
+                freq_set[item] += 1
+                local_set[item] += 1
 
-    for item, count in localSet.items():
-        support = float(count) / len(transactionList)
+    for item, count in local_set.items():
+        support = float(count) / len(transaction_list)
 
-        if support >= minSupport:
+        if support >= min_support:
             _itemSet.add(item)
 
     return _itemSet
 
 
-def joinSet(itemSet, length):
+def join_set(item_set, length):
     """Join a set with itself and returns the n-element itemsets"""
-    return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+    return set([i.union(j) for i in item_set for j in item_set if len(i.union(j)) == length])
 
 
-def getItemSetTransactionList(data_iterator):
-    transactionList = list()
-    itemSet = set()
+def get_item_set_transaction_list(data_iterator):
+    transaction_list = list()
+    item_set = set()
     for record in data_iterator:
         transaction = frozenset(record)
-        transactionList.append(transaction)
+        transaction_list.append(transaction)
         for item in transaction:
-            itemSet.add(frozenset([item]))  # Generate 1-itemSets
-    return itemSet, transactionList
+            item_set.add(frozenset([item]))  # Generate 1-itemSets
+    return item_set, transaction_list
 
 
-def runApriori(data_iter, minSupport, minConfidence):
+def apriori(data_iter, min_support, min_confidence):
     """
     run the apriori algorithm. data_iter is a record iterator
     Return both:
      - items (tuple, support)
      - rules ((pretuple, posttuple), confidence)
     """
-    itemSet, transactionList = getItemSetTransactionList(data_iter)
+    item_set, transaction_list = get_item_set_transaction_list(data_iter)
 
-    freqSet = defaultdict(int)
-    largeSet = dict()
+    freq_set = defaultdict(int)
+    large_set = dict()
     # Global dictionary which stores (key=n-itemSets,value=support)
     # which satisfy minSupport
 
     assocRules = dict()
     # Dictionary which stores Association Rules
 
-    oneCSet = returnItemsWithMinSupport(itemSet,
-                                        transactionList,
-                                        minSupport,
-                                        freqSet)
+    one_c_set = return_item_with_min_support(item_set,
+                                           transaction_list,
+                                           min_support,
+                                           freq_set)
 
-    currentLSet = oneCSet
+    current_l_set = one_c_set
     k = 2
-    while (currentLSet != set([])):
-        largeSet[k - 1] = currentLSet
-        currentLSet = joinSet(currentLSet, k)
-        currentCSet = returnItemsWithMinSupport(currentLSet,
-                                                transactionList,
-                                                minSupport,
-                                                freqSet)
-        currentLSet = currentCSet
+    while current_l_set != set([]):
+        large_set[k - 1] = current_l_set
+        current_l_set = join_set(current_l_set, k)
+        current_c_set = return_item_with_min_support(current_l_set,
+                                                   transaction_list,
+                                                   min_support,
+                                                   freq_set)
+        current_l_set = current_c_set
         k = k + 1
 
-    def getSupport(item):
+    def get_support(item):
         """local function which Returns the support of an item"""
-        return float(freqSet[item]) / len(transactionList)
+        return float(freq_set[item]) / len(transaction_list)
 
-    toRetItems = []
-    for key, value in largeSet.items():
-        toRetItems.extend([(tuple(item), getSupport(item))
+    to_ret_items = []
+    for key, value in large_set.items():
+        to_ret_items.extend([(tuple(item), get_support(item))
                            for item in value])
 
-    toRetRules = []
-    for key, value in largeSet.items()[1:]:
+    to_ret_rules = []
+    for key, value in large_set.items()[1:]:
         for item in value:
             _subsets = map(frozenset, [x for x in subsets(item)])
             for element in _subsets:
                 remain = item.difference(element)
                 if len(remain) > 0:
-                    confidence = getSupport(item) / getSupport(element)
-                    if confidence >= minConfidence:
-                        toRetRules.append(((tuple(element), tuple(remain)),
-                                           confidence))
-    return toRetItems, toRetRules
+                    confidence = get_support(item) / get_support(element)
+                    if confidence >= min_confidence:
+                        to_ret_rules.append(((tuple(element), tuple(remain)), confidence))
+    return to_ret_items, to_ret_rules
 
 
-def printResults(items, rules):
+def print_results(items, rules):
     """prints the generated itemsets sorted by support and the confidence rules sorted by confidence"""
     for item, support in sorted(items, key=lambda (item, support): support):
         print "item: %s , %.3f" % (str(item), support)
@@ -510,7 +508,7 @@ def printResults(items, rules):
         print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
 
 
-def dataFromFile(fname):
+def data_from_file(fname):
     """Function which reads from the file and yields a generator"""
     file_iter = open(fname, 'rU')
     for line in file_iter:
@@ -519,7 +517,7 @@ def dataFromFile(fname):
         yield record
 
 
-def run_apriori(logs_file_path='logs.json', min_supp=0.05, min_confidence=0.8):
+def run_apriori(logs_file_path, min_supp=0.05, min_confidence=0.8):
     hashes_temp = []
     table_csv = []
     date_time_obj0 = datetime.strptime('2018-01-01 00:00:00.0', '%Y-%m-%d %H:%M:%S.%f')
@@ -544,14 +542,14 @@ def run_apriori(logs_file_path='logs.json', min_supp=0.05, min_confidence=0.8):
     del table_csv[0]
 
     # Converts logs list to csv so that we can executre apriori algorithm on them
-    with open('inputApriori.csv', 'w') as fp:
+    with open('/opt/mycroft/habits/inputApriori.csv', 'w') as fp:
         writer = csv.writer(fp, delimiter=',')
         for row in table_csv:
             writer.writerow(row)
 
-    in_file = dataFromFile('inputApriori.csv')
+    in_file = data_from_file('/opt/mycroft/habits/inputApriori.csv')
 
-    items, rules = runApriori(in_file, min_supp, min_confidence)
+    items, rules = apriori(in_file, min_supp, min_confidence)
 
     # reformat the rules and sort the tuples in it
     hashes_temp = []
@@ -598,3 +596,4 @@ def run_apriori(logs_file_path='logs.json', min_supp=0.05, min_confidence=0.8):
         # register_habit("skill", intents)
         print intents
         intents = []
+    os.remove('/opt/mycroft/habits/inputApriori.csv')
