dbe6fb09ecb728f0525f0c248bed541f04373af8
Adrien Chevrier <adrien.chevrier.pro@gmail.com>
2018-02-03 17:12:59 +0100

    suppression pip

--- requirements.sh
+++ requirements.sh
@@ -6,9 +6,8 @@ if [ ! -d "/opt/mycroft/habits" ]; then
     echo '[]' > /opt/mycroft/habits/triggers.json
     sudo chmod -R ugo+rw /opt/mycroft/habits
 fi
-
-pip install --upgrade pip
-pip install -Iv scipy==0.13.3
-pip install -U scikit-learn
-pip install -U statistics
-pip install -U numpy
+#pip install --upgrade pip
+#pip install -Iv scipy==0.13.3
+#pip install -U scikit-learn
+#pip install -U statistics
+#pip install -U numpy
