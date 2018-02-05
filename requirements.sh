4343940cc71a1b4682162f8460b947211f4323ea
Gauthier Leonard <gauthier_leonard@msn.com>
2018-02-05 15:33:07 +0100

    Update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,8 @@
 #This file is for any packages you need installed for your skill to run
+source ~/.virtualenv/mycroft/bin/activate
 pip install --upgrade pip
 pip install -U numpy
 pip install -U scipy>=0.13.3
 pip install -U sklearn
 pip install -U statistics
+deactivate
