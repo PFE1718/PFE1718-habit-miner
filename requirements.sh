f70c624d6acf6fd481fa5e4832b7dbc22f21f381
Gauthier Leonard <gauthier_leonard@msn.com>
2018-02-05 14:53:15 +0100

    Update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,6 @@
 #This file is for any packages you need installed for your skill to run
 pip install --upgrade pip
+pip install -U numpy
+pip install -U scipy>=0.13.3
+pip install -U sklearn
+pip install -U statistics
