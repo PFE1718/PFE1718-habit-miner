7129194d33fbad862a158a5303c2f64c96f314e3
Gauthier Leonard <gauthier_leonard@msn.com>
2018-02-05 14:01:54 +0100

    Update requirements

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,6 @@
 #This file is for any packages you need installed for your skill to run
 sudo apt-get install python-pip
 pip install --upgrade pip
-#pip install --upgrade pip
 #pip install -Iv scipy==0.13.3
 #pip install -U scikit-learn
 #pip install -U statistics
